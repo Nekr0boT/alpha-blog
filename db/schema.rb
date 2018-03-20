1ce5512529474cad24b72de2317eee4fead56b10
Vitaliy Kindiy <vity.kind@gmail.com>
2018-03-20 20:02:00 +0200

    Prepare to production on heroku

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180317184937) do
+ActiveRecord::Schema.define(version: 20180320171521) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
