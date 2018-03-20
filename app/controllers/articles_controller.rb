cd3d806dc0c1ebcc19bfe754d6539fa0347df232
Vitaliy Kindiy <vity.kind@gmail.com>
2018-03-20 18:40:36 +0200

    Finish an first application

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,20 +1,51 @@
-class ArticlesController < ApllicationController
+class ArticlesController < ApplicationController
+	before_action :set_article, only: [:edit, :update, :destroy, :show]
+
+	def index
+		@articles = Article.all
+	end
 
 	def new
 		@article = Article.new
 	end
 
-	def create
-		#render plain: params[:article].inspect
+	def edit
+	end
 
+	def create
 		@article = Article.new(article_params)
-		@article.save
-		redirect_to articles_show(@article)
+		if @article.save
+			flash[:notice] = "Article was successfully created"
+			redirect_to article_path(@article)
+		else
+			render 'new'
+		end
+	end
+
+	def show
+	end
+
+	def destroy
+		@article.destroy
+		flash[:notice] = "Article was successfully deleted"
+		redirect_to articles_path
+	end
+
+	def update
+		if @article.update(article_params)
+			flash[:notice] = "This article was successfully updated"
+			redirect_to article_path(@article)
+		else
+			render 'edit'
+		end
 	end
 
 	private
+		def set_article
+			@article = Article.find(params[:id])
+		end
+
 		def article_params
 			params.require(:article).permit(:title, :description)
 		end
-
 end
